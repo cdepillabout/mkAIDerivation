a8f6a15270cb7419ff4a5983fd38776d82826da0
Dennis Gosnell <cdep.illabout@gmail.com>
2025-06-14 11:34:53 +0900

    Start adding some documentation

--- default.nix
+++ default.nix
@@ -1,3 +1,12 @@
+# This is a default.nix for easily playing around with mkAIDerivation.
+#
+# You can run this from the CLI like the following:
+#
+# $ nix-build --argstr msg 'Generate me a derivation for that silly `sl` program that prints a steam train on the terminal.'
+#
+# Make sure you have the ai-drv-server running in the background.
+#
+# If the resulting derivation builds correctly, it should be available in `./result/`.
 
 { msg ? null }:
 
--- flake.nix
+++ flake.nix
@@ -4,6 +4,8 @@
   inputs = {
     nixpkgs.url = "nixpkgs/nixpkgs-unstable";
 
+    # This provides the evilDownloadURL function, which allows (im)pure network
+    # access.
     evil-nix.url = "github:cdepillabout/evil-nix";
     evil-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
@@ -22,20 +24,50 @@
       overlay = final: prev:
         import ./nix/overlay.nix final prev //
         {
+          # This is an example of how to use the mkAIDerivation function.
           ai-drv-example =
             final.mkAIDerivation ''
               Generate me a derivation for that silly `sl` program that
               prints a steam train on the terminal.
-              You can download the source at the URL https://github.com/eyJhb/sl/archive/5.05.tar.gz
+              You can download the source at the URL
+              https://github.com/eyJhb/sl/archive/5.05.tar.gz
             '';
         };
 
+      # Expose the mkAIDerivation at the top-level.
+      #
+      # As you can see in the example above, the mkAIDerivation function
+      # takes a string describing the derivation you want to be generated,
+      # and returns the derivation.
+      #
+      # mkAIDerivation :: String -> Derivation
       mkAIDerivation = forAllSystems (system: nixpkgsFor.${system}.mkAIDerivation);
 
-      packages = forAllSystems (system: { inherit (nixpkgsFor.${system}) ai-drv-server ai-drv-example; });
+      packages =
+        forAllSystems
+          (system:
+            { inherit (nixpkgsFor.${system})
+                # The server used for interfacing with the OpenAI API.
+                ai-drv-server
+
+                # The above example of using the mkAIDerivation function.
+                ai-drv-example;
+            }
+          );
 
       defaultPackage = forAllSystems (system: self.packages.${system}.ai-drv-example);
 
+      # A dev shell for working on the ai-drv-server Python server.
       devShell = forAllSystems (system: nixpkgsFor.${system}.dev-shell);
+
+      defaultApp = forAllSystems (system: self.apps.${system}.ai-drv-server);
+
+      apps = forAllSystems (system: {
+        # This gives an easy way to run the ai-drv-server.
+        ai-drv-server = {
+          type = "app";
+          program = "${self.packages.${system}.ai-drv-server}/bin/ai-drv-server";
+        };
+      });
     };
 }
